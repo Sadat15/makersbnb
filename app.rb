33e061a4de7d8e256c0ec4d761c2b0c66a2e90d3
jovi <kwong.jovi@gmail.com>
2022-09-21 15:05:27 +0100

    Successful test of layout.erb with some test .erb files to demonstrate

--- app.rb
+++ app.rb
@@ -7,6 +7,18 @@ class Application < Sinatra::Base
   end
 
   get '/' do
-    return erb(:index)
+    erb :index
+  end
+
+  # ------------------
+  # testing layout.erb
+  # ------------------
+
+  get '/index_test' do
+    erb :index_test 
+  end
+  
+  get '/layout_test' do
+    erb :layout_test
   end
 end
