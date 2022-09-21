a50e181d2a12f1b17980a35ef58a85f9997bb75c
jovi <kwong.jovi@gmail.com>
2022-09-21 16:01:09 +0100

    Removed test files in /views as suggested. Removed lines corresponding lines in app.rb

--- app.rb
+++ app.rb
@@ -9,16 +9,4 @@ class Application < Sinatra::Base
   get '/' do
     erb :index
   end
-
-  # ------------------
-  # testing layout.erb
-  # ------------------
-
-  get '/index_test' do
-    erb :index_test 
-  end
-  
-  get '/layout_test' do
-    erb :layout_test
-  end
 end
